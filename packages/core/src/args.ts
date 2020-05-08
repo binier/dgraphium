e60bc639bb3420ea5e23ff681dd1ba08714985c7
Zurab Benashvili <zura.benashvili@gmail.com>
2020-05-08 17:03:31 +0400

    feat(core): add `Args`, contains: pagination, sorting, func

---
+++ packages/core/src/args.ts
@@ -0,0 +1,36 @@
+import { Uid } from './uid';
+
+export interface ArgsData {
+  func?: string;
+  first?: number;
+  offset?: number;
+  after?: Uid;
+}
+
+export type EdgeArgs = Omit<ArgsData, 'func'>;
+export type QueryArgs = ArgsData;
+
+/** func, pagination and sorting */
+export class Args {
+  constructor(
+    private args: ArgsData = {}
+  ) { }
+
+  setArg<K extends keyof ArgsData>(key: K, val: ArgsData[K]) {
+    this.args[key] = val;
+  }
+
+  length() {
+    return Object.values(this.args)
+      .filter(x => x)
+      .length;
+  }
+
+  toString() {
+    return Object.entries(this.args)
+      .filter(x => x[1])
+      .map(x => x.join(': '))
+      .join(', ')
+      .trim();
+  }
+}
