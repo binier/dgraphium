0027e2c7cdd8aac00735bddd8dfa345288d5f92c
Zura Benashvili <zura.benashvili@gmail.com>
2020-06-24 14:46:51 +0400

    feat(core): `Operator` can have `Uid` value

--- packages/core/src/operator/operator.ts
+++ packages/core/src/operator/operator.ts
@@ -1,7 +1,8 @@
 import { Param } from '../param';
+import { Uid } from '../uid';
 
 export type Subject = string;
-export type OpValue = Param | string | number | boolean;
+export type OpValue = Param | Uid | string | number | boolean;
 
 export interface OperatorArgs {
   name: string;
