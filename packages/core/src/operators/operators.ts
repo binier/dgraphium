6262e53977146707fd3d3e000b2b5b8aa1f96827
Zura Benashvili <zura.benashvili@gmail.com>
2020-06-24 23:52:20 +0400

    feat(core): implement `uid` operator

--- packages/core/src/operators/operators.ts
+++ packages/core/src/operators/operators.ts
@@ -1,4 +1,10 @@
 import { OperatorBuilder, Subject } from '../operator';
+import { UidLike, Uid } from '../uid';
+
+export const uid = (...uids: UidLike[]) => new OperatorBuilder({
+  name: 'uid',
+  value: uids.map(x => new Uid(x)),
+});
 
 export const has = (subject: Subject) =>
   new OperatorBuilder({ name: 'has', subject });
