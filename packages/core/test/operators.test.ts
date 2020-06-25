b97786e76a01012d690918387865a7341cf931b5
Zura Benashvili <zura.benashvili@gmail.com>
2020-06-25 12:44:39 +0400

    test(core): add basic tests for operators

---
+++ packages/core/test/operators.test.ts
@@ -0,0 +1,16 @@
+import { edge } from '../src';
+import { has } from '../src/operators';
+
+describe('Operators test', () => {
+  it('should not prefix by type if edge has none', () => {
+    expect(
+      edge({}).filter(has('myField')).toString()
+    ).toMatch(/has\(myField\)/)
+  });
+
+  it('should prefix by type if edge has one', () => {
+    expect(
+      edge('myType', {}).filter(has('myField')).toString()
+    ).toMatch(/has\(MyType\.myField\)/)
+  });
+});
