91a3b6ce1ae5fd691058fd4f547f3cd813fae793
Zura Benashvili <zura.benashvili@gmail.com>
2020-06-30 20:37:18 +0400

    test(core): operator `predUid` add param value tests

--- packages/core/test/operators/param-value.test.ts
+++ packages/core/test/operators/param-value.test.ts
@@ -1,5 +1,5 @@
 import { edge } from '../../src';
-import { uid } from '../../src/operators';
+import { uid, predUid } from '../../src/operators';
 import * as params from '../../src/param/param-types';
 
 describe('Operator test - Param value', () => {
@@ -29,4 +29,31 @@ describe('Operator test - Param value', () => {
     expect(myEdge.toString()).toMatch(/uid\(\$p1\)/);
     expect(myEdge.params()[0].getValue()).toEqual('[0x2, 0x3]');
   });
+
+  it('operator: `predUid` - single uid param', () => {
+    const myEdge = edge({}).filter(predUid('parent', params.uid('0x2')));
+    expect(myEdge.toString()).toMatch(/uid_in\(parent, \$p1\)/);
+    expect(myEdge.build().params()[0].getValue())
+      .toEqual('0x2');
+  });
+
+  // it('operator: `predUid` - multi uid in separate params', () => {
+  //   const myEdge = edge({}).filter(
+  //     predUid('parent', [params.uid('0x2'), params.uid('0x3')])
+  //   ).build();
+  //   const myParams = myEdge.params();
+
+  //   expect(myEdge.toString()).toMatch(/uid_in\(parent, \$p1, \$p2\)/);
+  //   expect(myParams[0].getValue()).toEqual('0x2');
+  //   expect(myParams[1].getValue()).toEqual('0x3');
+  // });
+
+  // it('operator: `predUid` - multi uid in same param', () => {
+  //   const myEdge = edge({}).filter(
+  //     predUid('parent', params.uids('0x2', '0x3'))
+  //   ).build();
+
+  //   expect(myEdge.toString()).toMatch(/uid_in\(parent, \$p1\)/);
+  //   expect(myEdge.params()[0].getValue()).toEqual('[0x2, 0x3]');
+  // });
 });
