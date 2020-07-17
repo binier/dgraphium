da333b446ea84f7d21f26c9c33f0c14700d57840
Zura Benashvili <zura.benashvili@gmail.com>
2020-07-17 13:52:57 +0400

    test(core): add `@cascade` directive tests

---
+++ packages/core/test/directive.test.ts
@@ -0,0 +1,14 @@
+import { edge } from '../src';
+import { has } from '../src/operators';
+
+// does not test formatting
+describe('Directive test', () => {
+  it('`@cascade` directive', () => {
+    expect(edge({}).cascade().toString())
+      .toMatch(/@cascade \{/);
+
+    expect(
+      edge({}).filter(has('name')).cascade().toString()
+    ).toMatch(/@filter\(has\(name\)\) @cascade \{/);
+  });
+});
