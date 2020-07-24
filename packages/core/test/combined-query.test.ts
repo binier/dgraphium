00139dc57c2140bb9ebdf35ea659a33eb87a3f8c
Zura Benashvili <zura.benashvili@gmail.com>
2020-07-24 17:43:27 +0400

    test(core): combining combined queries

--- packages/core/test/combined-query.test.ts
+++ packages/core/test/combined-query.test.ts
@@ -20,6 +20,22 @@ describe('Query test', () => {
     expect(q.toString()).toMatch(/q2\(func: uid\(\$p2\)\)/);
   });
 
+  it('combining combined queries', () => {
+    const uidParam1 = params.uid('0x2');
+    const uidParam2 = params.uid('0x3');
+    const q = combined(
+      combined(
+        query('user').func(uid(uidParam1)),
+        query('post').func(uid(uidParam2))
+      ),
+      query('user').func(uid(uidParam1))
+    ).build();
+
+    expect(q.toString()).toMatch(/q1\(func: uid\(\$p1\)\)/);
+    expect(q.toString()).toMatch(/q2\(func: uid\(\$p2\)\)/);
+    expect(q.toString()).toMatch(/q3\(func: uid\(\$p1\)\)/);
+  });
+
   it('should reuse unique param name if same parameter', () => {
     const uidParam = params.uid('0x2');
     const q = combined(
