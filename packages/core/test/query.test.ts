1fa0e985b4cad388edeeb3176786c2f5e113b798
Zura Benashvili <zura.benashvili@gmail.com>
2020-07-30 13:24:09 +0400

    feat(core): add projection merging tests

--- packages/core/test/query.test.ts
+++ packages/core/test/query.test.ts
@@ -1,4 +1,14 @@
-import { query } from '../src';
+import { query, EdgeBuilder } from '../src';
+
+function edgesToObject(q: EdgeBuilder) {
+  return Object.entries(q['edges'])
+    .reduce((r, [k, v]) => {
+      if (v instanceof EdgeBuilder)
+        return { ...r, [k]: edgesToObject(v) };
+      r[k] = v;
+      return r;
+    }, {});
+}
 
 describe('Query test', () => {
   it('should insert queryName in stringified query', () => {
@@ -12,5 +22,49 @@ describe('Query test', () => {
     expect(
       query('user').toString()
     ).toMatch(/q1\(.*\)/);
+  });
+
+  it('query.project should deep merge projections by default', () => {
+    const q = query()
+      .project({
+        a: 1,
+        b: {
+          c: 1,
+          d: 1,
+        },
+      })
+      .project({
+        a: 0,
+        b: {
+          c: 0,
+        },
+      });
+
+    expect(edgesToObject(q)).toMatchObject({
+      a: 0,
+      b: {
+        c: 0,
+        d: 1,
+      },
+    });
+  });
+
+  it('query.project should overwrite projections if flag is true', () => {
+    const q = query()
+      .project({
+        a: 1,
+        b: {
+          c: 1,
+          d: 1,
+        },
+      })
+      .project({
+        a: 0,
+      }, true);
+
+    const edges = edgesToObject(q);
+
+    expect(edges.a).toEqual(0);
+    expect(edges.b).toEqual(undefined)
   });
 });
