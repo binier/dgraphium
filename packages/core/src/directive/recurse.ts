b86943501f5c43e93659b570e3969bb92a0e3778
Zura Benashvili <zura.benashvili@gmail.com>
2020-11-08 20:59:49 +0400

    docs(core/directive/recurse): fix docs not visible in ide

--- packages/core/src/directive/recurse.ts
+++ packages/core/src/directive/recurse.ts
@@ -1,11 +1,14 @@
 
 import { Param, ParamBuilder } from '../param';
 
-// @see https://dgraph.io/docs/query-language/recurse-query/
+/** @see https://dgraph.io/docs/query-language/recurse-query/ */
 export type RecurseBuilderArgs = undefined | {
-  // The loop parameter can be set to false, in which case paths which lead to a loop would be ignored while traversing.
+  /**
+   * The **loop** parameter can be set to `false`, in which case
+   * paths which lead to a loop would be ignored while traversing.
+   * */
   loop?: boolean | ParamBuilder<'boolean'>;
-  // the maximum depth to recurse.
+  /** the maximum depth to recurse. */
   depth?: number | ParamBuilder<'int'>;
 }
 
